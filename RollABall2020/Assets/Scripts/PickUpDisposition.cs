a59bff0ff464377e2f367c689f6fe8ed85c9d266
magur0don <magurodon.10000yen@gmail.com>
2020-07-04 14:20:55 +0900

    firstComit

---
+++ RollABall2020/Assets/Scripts/PickUpDisposition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpDisposition : MonoBehaviour
+{
+    public enum PublicPickUpDisposition
+    {
+        None = 0,       //何もない
+        SpeedUp,        //加速する
+        SpeedDown,      //減速する
+        QTE,            //QTEが始まる
+        DispositionMax  //性質数のMAX
+    }
+    
+    public PublicPickUpDisposition m_pickUpDisposition = PublicPickUpDisposition.None;
+
+    private void Awake()
+    {
+        var randomDisposition = 0;
+
+        randomDisposition = Random.Range((int)PublicPickUpDisposition.None, (int)PublicPickUpDisposition.DispositionMax);
+
+        m_pickUpDisposition = (PublicPickUpDisposition)randomDisposition;
+    }
+}
---
+++ RollABall2020/Assets/Scripts/PickUpDispositionEffect.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PickUpDispositionEffect : MonoBehaviour
+{
+    private PickUpDisposition m_pickUpDisposition = null;
+
+    private QTEPresenter m_QTEPresenter = null;
+
+    private void Start()
+    {
+        m_pickUpDisposition = GetComponent<PickUpDisposition>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DispositionEffect(other);
+        }
+    }
+    
+    private void DispositionEffect(Collider other)
+    {
+        var vec = Vector3.Normalize(transform.position - other.transform.position);
+
+        switch (m_pickUpDisposition.m_pickUpDisposition)
+        {
+            case PickUpDisposition.PublicPickUpDisposition.SpeedUp:
+                other.GetComponent<Rigidbody>().AddForce(vec * 5f, ForceMode.Impulse);
+                break;
+            case PickUpDisposition.PublicPickUpDisposition.SpeedDown:
+                other.GetComponent<Rigidbody>().AddForce(vec * -100f, ForceMode.Acceleration);
+                break;
+            case PickUpDisposition.PublicPickUpDisposition.QTE:
+                if (m_QTEPresenter == null)
+                {
+                    m_QTEPresenter = GameObject.Find("Canvas").GetComponent<QTEPresenter>();
+                }
+                m_QTEPresenter.SphereDamage = other.GetComponent<SphereDamage>();
+                m_QTEPresenter.QTEPresenterStates = QTEPresenter.QTEStates.ON;
+                break;
+        }
+    }
+}
